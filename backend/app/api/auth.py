3838874d4c298296b019cf62885c09b685977c14
riroan <riroan@naver.com>
2024-04-21 03:01:53 +0900

    회원가입 시 암호화 적용

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -1,3 +1,4 @@
+import bcrypt
 from app.depends.common import get_session
 from app.dto.auth import UserDTO
 from app.response import CREATED
@@ -12,10 +13,12 @@ api = APIRouter()
 async def register(user_info: UserDTO, session: Session = Depends(get_session)):
     user_repository = UserRepository(session)
 
+    encrypted_password = bcrypt.hashpw(user_info.password.encode(), bcrypt.gensalt())
+
     user_repository.save(
         nickname=user_info.nickname,
         email=user_info.email,
-        password=user_info.password,
+        password=encrypted_password,
         gender=user_info.gender,
         birthday=user_info.birthday,
     )
